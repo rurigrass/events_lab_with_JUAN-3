cf0db7b2d432bbef8d0e5bc112cce14e79309b5c
rurigrass <ruripg@gmail.com>
2018-06-21 16:27:19 +0100

    books

---
+++ lab_events_start/js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+const readingList = document.querySelector('#reading-list')
+const form = document.querySelector('#new-item-form')
+
+const handleFormSave = function(event) {
+  event.preventDefault();
+  const form = event.target;
+  const title = form.title.value;
+  const author = form.author.value;
+  const category = form.category.value;
+console.log(category);
+  const result = document.querySelector('#reading-list');
+  // result.textContent = `Title: ${title}, Author: ${author}, Category: ${category}`
+
+  const paragraph = document.createElement('div');
+  paragraph.textContent = `Title: ${title}, Author: ${author}, Category: ${category}`
+  result.appendChild(paragraph);
+
+  form.reset()
+}
+
+form.addEventListener('submit', handleFormSave)
+
+// const createParagraph = function (textContent, container) {
+//   const paragraph = document.createElement('p');
+//   paragraph.textContent = textContent;
+//   container.appendChild(paragraph)
+// }
+
+});
